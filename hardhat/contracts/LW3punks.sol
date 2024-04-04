5e30850e19929b533c71fad50bfb6d1dc3171cbe
Tushar Nagar <nagartushar771@gmail.com>
2024-04-04 22:01:12 +0530

    project ipfs-nft init

---
+++ hardhat/contracts/LW3punks.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract IPFSNft {
+
+    constructor () ERC721("name", "symbol"){
+        
+    }
+
+}
